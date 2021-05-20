8db6f626114280ca621468222343d74cc3d8b13d
kumarthapa <kumarthapa@kumarthapa>
2021-05-20 14:48:36 +0530

    add new text

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+const App = () => {
+  return (
+    <>
+      <div className="container" >
+        <p>hi</p>
+
+       
+      </div>
+    </>
+  );
+};
+
+export default App;
